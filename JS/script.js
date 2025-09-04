99692abab9bebd532a5b587c92aa8d9c087547c4
L33hxfm <151097168+L33hxfm@users.noreply.github.com>
2025-09-03 22:49:42 -0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -18,7 +18,7 @@ function login() {
 
     if (localStorage.getItem(email) === senha) {
         localStorage.setItem("logado", email);
-        window.location.href = "pages/diario.html";
+        window.location.href = "./pages/diario.html";
     } else {
         alert("Usuário ou senha incorretos!");
     }
@@ -93,5 +93,6 @@ function excluirConta() {
 
 // Carregar automaticamente quando abre o diário
 document.addEventListener("DOMContentLoaded", mostrarEntradas);
+
 
 
